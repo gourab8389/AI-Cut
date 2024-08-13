fd24f31c3e2446e784885ca0ca1b70835bd2e66f
gourab8389 <deyg6988@gmail.com>
2024-08-14 00:06:35 +0530

    chore: Update landing page design and styles

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,9 @@
 import express from "express";
+
+const app = express();
+
+app.get('/test', (req, res)=> {
+    return res.json('ok');
+});
+
+app.listen(8000, ()=> console.log('listening on port 8000'))
